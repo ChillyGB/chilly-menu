4230c08555dea10dfff8c0664e41c5a985769ebd
AuroraViola <110638310+AuroraViola@users.noreply.github.com>
2025-04-20 23:28:52 +0200

    Implemented CGB mode

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ rgbgfx -o ./inc/img/qr_code_tiles.2bpp ./inc/img/qr_code.png
 function build_rom () {
     rgbasm -o "./build/obj/$1.o" "./src/$1.asm"
     rgblink -o "./build/$1.gb" "./build/obj/$1.o"
-    rgbfix -v -p 0xFF -m 0x57 "./build/$1.gb"
+    rgbfix -v -c -p 0xFF -m 0x57 "./build/$1.gb"
 }
 
 build_rom "boot"
--- src/boot.asm
+++ src/boot.asm
@@ -18,6 +18,15 @@ SECTION "Header", ROM0[$100]
 
 entry_point:
 	ld sp, $dfff
+	ld hl, IS_GBC
+	cp $11
+	jp nz, not_gbc
+
+	ld [hl], 1
+	jr end_gbc_check
+not_gbc:
+	ld [hl], 0
+end_gbc_check:
 
 	; Turn PPU and APU off
 	wait_vblank
@@ -72,16 +81,54 @@ entry_point:
 	; Write tiles
 	memcpy $8000, char_tiles, char_tiles.end - char_tiles
 	memcpy $9000, cheat_char_tiles, cheat_char_tiles.end - cheat_char_tiles
+	; Set colors
+	ld a, 1
+	ld [rVBK], a
+	memset $9800, $20, 1
+	memset $9a20, $20, 1
+	ld a, 0
+	ld [rVBK], a
 	; Write qr tiles
 	memcpy $8800, qr_tiles, qr_tiles.end - qr_tiles
 	; Load game_list_loader to WRAM
 	memcpy $c200, game_list_loader_ram, game_list_loader_ram.end - game_list_loader_ram
 
 	; Initialize registers
+	ld hl, IS_GBC
+	ld a, [hl]
+	and a
+	jp nz, init_cgb_palettes
 	ld a, %11100100
 	ld [rBGP], a
 	ld [rOBP0], a
 	ld [rOBP1], a
+	jp end_init_palettes
+init_cgb_palettes:
+	set_palette_bg $ff, $ff, $ff, 0
+	set_palette_bg $63, $a5, $ff, 1
+	set_palette_bg 0, $63, $c5, 2
+	set_palette_bg 0, 0, 0, 3
+
+	set_palette_bg $ff, $ff, $ff, 4
+	set_palette_bg $7b, $ff, $31, 5
+	set_palette_bg 0, $63, $c5, 6
+	set_palette_bg 0, 0, 0, 7
+
+	set_palette_bg $21, $b1, $ff, 8
+	set_palette_bg $63, $a5, $ff, 9
+	set_palette_bg 0, 0, $ff, 10
+	set_palette_bg 0, 0, 0, 11
+
+	set_palette_bg $ff, $21, $8c, 12
+	set_palette_bg $63, $a5, $ff, 13
+	set_palette_bg 0, 0, $ff, 14
+	set_palette_bg 0, 0, 0, 15
+
+	set_palette_obj $ff, $ff, $ff, 0
+	set_palette_obj $ff, $21, $8c, 1
+	set_palette_obj 0, $63, $c5, 2
+	set_palette_obj 0, 0, 0, 3
+end_init_palettes:
 
 	ld a, 31
 	ld [rLYC], a
